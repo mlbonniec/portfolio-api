5c2acf842da9ca13e3885f83d3166052601f7ae0
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-07-26 16:19:39 +0200

    docs(projects): add OpenAPI swagger decorators

--- src/objects/projects/project.entity.ts
+++ src/objects/projects/project.entity.ts
@@ -1,12 +1,15 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 import { ObjectId } from '@mikro-orm/mongodb';
 import { GlobalRepository } from '@/repositories/global.repository';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ collection: 'projects', customRepository: () => GlobalRepository })
 export class ProjectEntity {
+  @ApiProperty({ description: 'The id of the project', name: 'id', type: 'string' })
   @PrimaryKey()
   readonly _id: ObjectId;
 
+  @ApiProperty({ description: 'The name of the project' })
   @Property()
   name: string;
 }
--- src/routes/projects/projects.controller.ts
+++ src/routes/projects/projects.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { ProjectsService } from '@routes/projects/projects.service';
 import { ProjectEntity } from '@objects/projects/project.entity';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { DocumentationTags } from '@config/swagger.config';
 
+@ApiTags(DocumentationTags.PROJECTS)
 @Controller({ path: 'projects', version: '1' })
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
+  @ApiOperation({ summary: 'Get all the projects.' })
+  @ApiOkResponse({ type: ProjectEntity, isArray: true, description: 'Projects found.' })
   @Get()
   getAllProjects(): Promise<ProjectEntity[]> {
     return this.projectsService.getAllProjects();
