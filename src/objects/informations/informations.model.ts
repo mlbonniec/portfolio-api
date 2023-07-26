87918b0a41a3e0c93ad3f9c2afc2ef06cfc94cb2
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-07-26 14:12:12 +0200

    docs(informations): add OpenAPI decorators for InformationsViewModel

--- src/objects/informations/informations.model.ts
+++ src/objects/informations/informations.model.ts
@@ -1,9 +1,19 @@
-import type { Environment } from '@/types/environments';
+import { Environment } from '@/types/environments';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class InformationsViewModel {
+  @ApiProperty({ example: '1.0.0' })
   version: string;
+
+  @ApiProperty({ example: 'Portfolio API.' })
   name: string;
+
+  @ApiProperty({ example: 'Portfolio API made in NestJS.' })
   description: string;
+
+  @ApiProperty({ enum: Environment, example: Environment.DEVELOPMENT })
   environment: Environment;
+
+  @ApiProperty({ required: false, example: 8080 })
   port?: number;
 }
