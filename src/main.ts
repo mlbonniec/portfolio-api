575106b5aef7c8ae6413c4bd25e7ac35fd4528ef
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-07-26 10:55:32 +0200

    refactor(main): generate swagger documentation and add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import type { OpenAPIObject } from '@nestjs/swagger';
+import type { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { validationPipeConfig } from '@config/validation-pipe.config';
+import { EnvironmentHelper } from '@helpers/environment';
+import { Environment } from '@/types/environments';
+import { SwaggerModule } from '@nestjs/swagger';
+import { customOptions, documentBuilder } from '@config/swagger.config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+(async function (): Promise<void> {
+  const app: INestApplication = await NestFactory.create(AppModule);
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, documentBuilder);
+
+  switch (EnvironmentHelper.getEnvironment()) {
+    case Environment.DEVELOPMENT:
+      SwaggerModule.setup('docs', app, document, customOptions);
+      break;
+    default:
+      break;
+  }
+
+  app.useGlobalPipes(new ValidationPipe(validationPipeConfig));
+
+  await app.listen(EnvironmentHelper.getPort());
+})();
