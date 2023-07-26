d6ff8312bfc3c2e22f059f542717e3df0afbb227
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-07-26 10:52:59 +0200

    feat(swagger): create a configuration for the swagger documentation

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,25 @@
+import { DocumentBuilder, OpenAPIObject, SwaggerCustomOptions } from '@nestjs/swagger';
+import { SwaggerTheme } from 'swagger-themes';
+
+export enum DocumentationTags {
+  INFORMATIONS = 'Informations',
+  PROJECTS = 'Projects',
+}
+
+export const documentBuilder: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+  .setTitle('Mathis LE BONNIEC - Portfolio API')
+  .setDescription('The API for Mathis LE BONNIEC porfolio.')
+  .setVersion('1.0')
+  .addTag(DocumentationTags.INFORMATIONS, 'Get the API informations.')
+  .addTag(DocumentationTags.PROJECTS, 'Manages all the projects of the application.')
+  .setContact('Mathis LE BONNIEC', 'https://github.com/mlbonniec', 'mathislbonniec@gmail.com')
+  .build();
+
+export const customOptions: SwaggerCustomOptions = {
+  customSiteTitle: 'Mathis LE BONNIEC - API Documentation',
+  customCss: `
+  @media (prefers-color-scheme: dark) {
+    ${new SwaggerTheme('v3').getBuffer('dark')}
+  }
+  `
+}
