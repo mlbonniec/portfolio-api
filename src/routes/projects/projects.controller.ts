eeb6adffebae6f65ac4008285dec3ce372bff5b8
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-07-27 23:01:01 +0200

    feat(project): create post route and service to create new projects

--- src/routes/projects/projects.controller.ts
+++ src/routes/projects/projects.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProjectsService } from '@routes/projects/projects.service';
 import { ProjectEntity } from '@objects/projects/project.entity';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { DocumentationTags } from '@config/swagger.config';
+import { CreateProjectDto } from '@objects/projects/project.dto';
 
 @ApiTags(DocumentationTags.PROJECTS)
 @Controller({ path: 'projects', version: '1' })
@@ -14,5 +15,12 @@ export class ProjectsController {
   @Get()
   getAllProjects(): Promise<ProjectEntity[]> {
     return this.projectsService.getAllProjects();
+  }
+
+  @ApiOperation({ summary: 'Create a project.' })
+  @ApiOkResponse({ type: ProjectEntity, description: 'Project created.' })
+  @Post()
+  async create(@Body() body: CreateProjectDto): Promise<ProjectEntity> {
+    return this.projectsService.create(body);
   }
 }
--- src/routes/projects/projects.service.ts
+++ src/routes/projects/projects.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { ProjectEntity } from '@objects/projects/project.entity';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { GlobalRepository } from '@/repositories/global.repository';
+import { CreateProjectDto } from '@objects/projects/project.dto';
+import { Exceptions } from '@helpers/exceptions';
 
 @Injectable()
 export class ProjectsService {
@@ -9,7 +11,29 @@ export class ProjectsService {
     @InjectRepository(ProjectEntity.name) private readonly projectsRepository: GlobalRepository<ProjectEntity>
   ) {}
 
+  /**
+   * Get all the projects.
+   * @returns {Promise<ProjectEntity[]>} The projects found.
+   */
   async getAllProjects(): Promise<ProjectEntity[]> {
     return this.projectsRepository.findAll();
+  }
+
+  /**
+   * Create a project.
+   * @param {CreateProjectDto} projectDto The project to create.
+   * @throws {Exception} If the project cannot be created.
+   * @returns {Promise<ProjectEntity>} The project created.
+   */
+  async create(projectDto: CreateProjectDto): Promise<ProjectEntity> {
+    const project: ProjectEntity = this.projectsRepository.create<ProjectEntity>(projectDto);
+
+    try {
+      await this.projectsRepository.insert(project);
+    } catch (error) {
+      Exceptions.INTERNAL_SERVER_ERROR.throw();
+    }
+
+    return project;
   }
 }
